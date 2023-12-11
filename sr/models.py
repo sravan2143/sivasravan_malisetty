8c68624e2381186fce219f259b8c1e1d75ea60c9
sravan2143 <sivasravanmalisetty@gmail.com>
2023-12-11 15:56:24 -0800

    changes1

---
+++ sr/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.name
